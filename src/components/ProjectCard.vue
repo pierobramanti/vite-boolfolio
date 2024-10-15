b9b9894182572f186f39677f2380fbfaa1e1b87a
pierobramanti <piero.bramanti94@gmail.com>
2024-10-15 17:32:33 +0200

    [UPD] Display project description and technologies in ProjectCard

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -13,7 +13,13 @@ export default {
           <img :src="project.image ? `http://127.0.0.1:8000/storage/${project.image}` : 'https://picsum.photos/200/200'" alt="Project Image" class="card-img-top">
           <div class="card-body">
             <h5 class="card-title">{{ project.title }}</h5>
+            <p v-if="project.type"><strong>Type:</strong> {{ project.type.name }}</p>
             <p class="card-text">{{ project.description }}</p>
+            <ul v-if="project.technologies" class="list-unstyled d-flex flex-wrap gap-2">
+                <li v-for="tech in project.technologies" :key="tech.id" class="badge bg-primary">
+                  {{ tech.name }}
+                </li>
+            </ul>
           </div>
           <div class="card-footer">
             <small class="text-muted">Date: {{ project.date }}</small>
