e88fc06777e34d3cfcaa3fa76db5cbc10ee6da8e
pierobramanti <piero.bramanti94@gmail.com>
2024-10-15 15:44:13 +0200

    [UPD] Milestone 4 completed: Axios request to receive data from backend

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,38 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      
+      projects: []
     }
   },
-    created() {
-      
-    },
-    methods: {
-      
+  created() {
+    this.getAllProjects(); 
+  },
+  methods: {
+    getAllProjects() {  
+      axios.get('http://127.0.0.1:8000/api/projects')  
+        .then(response => {
+          console.log(response.data)
+        })
+        
     }
+  }
 }
 </script>
 
 <template>
-
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center"> Elenco Progetti</h1>
+      </div>
+      <div class="col-12">
+        <!-- ELENCO DEI PROGETTI-->
+        
+      </div>
+    </div>
+  </div>
 </template>
 
-<style lang=scss"">
-
+<style lang="scss">
+@import './styles/generals.scss';
 </style>
