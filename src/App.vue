ecb7fe6f5fca5abfd4eedbcfa1a34ce668279c26
pierobramanti <piero.bramanti94@gmail.com>
2024-10-15 15:56:27 +0200

    [UPD] created card structure

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ export default {
     getAllProjects() {  
       axios.get('http://127.0.0.1:8000/api/projects')  
         .then(response => {
-          console.log(response.data)
+          this.projects=response.data.results
         })
         
     }
@@ -28,9 +28,22 @@ export default {
         <h1 class="text-center"> Elenco Progetti</h1>
       </div>
       <div class="col-12">
-        <!-- ELENCO DEI PROGETTI-->
-        
-      </div>
+        <!-- ELENCO DEI PROGETTI -->
+        <div class="row">
+          <div class="col-12 col-md-6 col-lg-4">
+            <div class="card">
+              <img src="" alt="Project Image" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">titolo</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste hic, vitae maxime porro iusto eum iure. Minima distinctio expedita ea, labore necessitatibus, dolores atque nemo repellendus fugit quisquam numquam possimus.</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted"></small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>      
     </div>
   </div>
 </template>
