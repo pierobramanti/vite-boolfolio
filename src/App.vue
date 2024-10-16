2fa2284623ae502cedfc3278a7f2e8330238955f
pierobramanti <piero.bramanti94@gmail.com>
2024-10-16 22:33:44 +0200

    [UPD]  integrate router import in main.js and styled header section

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,24 @@
 <script>
-import ProjectCard from './components/ProjectCard.vue';
 import AppHeader from './components/AppHeader.vue';
 
 import axios from 'axios';
 
 export default {
   components:{
-    ProjectCard,
     AppHeader
   },
   data() {
     return {
-      projects: [],
-      last_page: null,
-      current_page:null,
-    }
-  },
-  created() {
-    this.getAllProjects(); 
-  },
-  methods: {
-    getAllProjects() {  
-      axios.get('http://127.0.0.1:8000/api/projects')  
-        .then(response => {
-          this.projects = response.data.results.data; 
-          this.last_page = response.data.results.last_page;
-          this.current_page = response.data.results.current_page;
-        });
-    },
-    goToPage(page){
-      axios.get('http://127.0.0.1:8000/api/projects?page=' + page)  
-        .then(response => {
-          this.projects = response.data.results.data; 
-          this.current_page = response.data.results.current_page;
-        });
+    
     }
   }
 }
 </script>
 
 <template>
-  <AppHeader/>
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <h1 class="text-center">Elenco Progetti</h1>
-      </div>
-      <div class="col-12">
-        <!-- ELENCO DEI PROGETTI -->
-        <div class="row gy-4">
-          <ProjectCard v-for="proj in projects" :key="proj.id" :project="proj"/>
-        </div>
-      </div>  
-      <nav aria-label="Page navigation example" class="mt-4">
-        <ul class="pagination d-flex justify-content-center">
-          <li class="page-item"><a class="page-link" href="#" @click="goToPage(current_page - 1)">Previous</a></li>
-          <li class="page-item" v-for="i in last_page"><a class="page-link" href="#" @click="goToPage(i)">{{i}}</a></li>
-          <li class="page-item"><a class="page-link" href="#" @click="goToPage(current_page + 1)">Next</a></li>
-        </ul>
-      </nav>    
-    </div>
-</div>
+<AppHeader/>
+<router-view></router-view>
+
 
 </template>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,18 +5,18 @@ export default {
 </script>
 
 <template >
-    <header>
+    <header class="header">
         <div class="container">
             <div class="row">
                 <div class="col-12 d-flex justify-content-between align-items-center py-3">
-                    <ul class="list-unstyled d-flex mb-0 gap-3">
-                        <li><a href="#" class="text-decoration-none">Home</a></li>
-                        <li><a href="#" class="text-decoration-none">Projects</a></li>
+                    <ul class="nav-list">
+                        <li><a href="#" class="nav-link">Home</a></li>
+                        <li><a href="#" class="nav-link">Projects</a></li>
                     </ul>
                 </div>
             </div>
         </div>
-    </header> 
+    </header>
 </template>
 <style lang="">
     
