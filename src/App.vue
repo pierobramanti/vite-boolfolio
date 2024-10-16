a3e31165b819e7009f6318406cafd300650a46d3
pierobramanti <piero.bramanti94@gmail.com>
2024-10-16 18:05:32 +0200

    [UPD] Added pagination controls to navigate through project pages

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,9 @@ export default {
   },
   data() {
     return {
-      projects: []
+      projects: [],
+      last_page: null,
+      current_page:null,
     }
   },
   created() {
@@ -18,7 +20,16 @@ export default {
     getAllProjects() {  
       axios.get('http://127.0.0.1:8000/api/projects')  
         .then(response => {
-          this.projects = response.data.results; 
+          this.projects = response.data.results.data; 
+          this.last_page = response.data.results.last_page;
+          this.current_page = response.data.results.current_page;
+        });
+    },
+    goToPage(page){
+      axios.get('http://127.0.0.1:8000/api/projects?page=' + page)  
+        .then(response => {
+          this.projects = response.data.results.data; 
+          this.current_page = response.data.results.current_page;
         });
     }
   }
@@ -29,16 +40,24 @@ export default {
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <h1 class="text-center"> Elenco Progetti</h1>
+        <h1 class="text-center">Elenco Progetti</h1>
       </div>
       <div class="col-12">
         <!-- ELENCO DEI PROGETTI -->
         <div class="row gy-4">
-          <ProjectCard  v-for="proj in projects" :key="proj.id" :project="proj"/>
+          <ProjectCard v-for="proj in projects" :key="proj.id" :project="proj"/>
         </div>
-      </div>      
+      </div>  
+      <nav aria-label="Page navigation example" class="mt-4">
+        <ul class="pagination d-flex justify-content-center">
+          <li class="page-item"><a class="page-link" href="#" @click="goToPage(current_page - 1)">Previous</a></li>
+          <li class="page-item" v-for="i in last_page"><a class="page-link" href="#" @click="goToPage(i)">{{i}}</a></li>
+          <li class="page-item"><a class="page-link" href="#" @click="goToPage(current_page + 1)">Next</a></li>
+        </ul>
+      </nav>    
     </div>
-  </div>
+</div>
+
 </template>
 
 <style lang="scss">
