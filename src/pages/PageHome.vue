f7e673418399c9f7e1a3ff7c46b4d168535e0c79
pierobramanti <piero.bramanti94@gmail.com>
2024-10-16 18:47:24 +0200

    [UPD] created PageHome.vue

---
+++ src/pages/PageHome.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    
+}
+</script>
+<template >
+    <div class="main py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center mb-4">Benvenuti sul sito dei progetti di Piero Bramanti</h1>
+                    <p class="text-center lead">
+                        Questo sito raccoglie e presenta i progetti realizzati da Piero Bramanti durante il corso di Fullstack Web Developer presso Boolean. Qui troverete una varietà di lavori che mostrano le competenze acquisite nel corso, includendo sviluppo frontend e backend. Esplorate i progetti per vedere in azione le tecnologie più moderne e le soluzioni creative adottate per risolvere diverse sfide di sviluppo web.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</template>
+<style >
+    
+</style>
