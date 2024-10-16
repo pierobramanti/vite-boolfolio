feb375ad98edcf9f80dc6668e795fb64e42ff90e
pierobramanti <piero.bramanti94@gmail.com>
2024-10-16 18:23:51 +0200

    [UPD] created AppHeader.vue

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template >
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-between align-items-center py-3">
+                    <ul class="list-unstyled d-flex mb-0 gap-3">
+                        <li><a href="#" class="text-decoration-none">Home</a></li>
+                        <li><a href="#" class="text-decoration-none">Projects</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header> 
+</template>
+<style lang="">
+    
+</style>
