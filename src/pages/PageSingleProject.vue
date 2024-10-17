6b0cf657c6c5aa12e744aae0a399984617e6dcb2
pierobramanti <piero.bramanti94@gmail.com>
2024-10-17 16:30:38 +0200

    [UPD] add axios request and project display in PagesingleProject component

---
+++ src/pages/PageSingleProject.vue
@@ -0,0 +1,47 @@
+<script>
+import axios from 'axios';
+import { store } from '../store.js'; 
+
+export default {
+    data(){
+        return{
+            store,
+            project: null,
+        }
+    },
+    methods:{
+        getProject(){
+            axios.get('INDIRIZZO').then(res => {  
+                if(res.data.success){
+                    this.project = res.data.results;
+                }
+            });
+        }
+    }
+}
+</script>
+
+<template>
+   <div class="container">
+    <div class="row">
+        <div class="col-12">
+            <img src="" alt="">
+        </div>
+        <div class="col-12">
+            <div class="content">
+
+            </div>
+            <h1>Titolo</h1>
+            <p><strong>Tipologia:</strong></p>
+            <p><strong>Tecnologia:</strong></p> 
+        </div>
+        <div class="col-12">
+
+        </div>
+    </div>
+   </div>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
