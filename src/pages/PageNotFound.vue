1e9e8674815631bc29a2a0789e99cc72366e6c0f
pierobramanti <piero.bramanti94@gmail.com>
2024-10-19 17:20:43 +0200

    [UPD] created PageNotFound

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template >
+  <div class="containe">
+    <div class="row">
+        <div class="col-12">
+            <div class="text-center">
+                <h1>Pagina non trovata</h1>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+    
+</style>
